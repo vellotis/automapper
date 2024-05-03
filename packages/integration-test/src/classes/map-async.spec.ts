1446a8e8f9579aba2d80db635f2ff0ef8f6fb129
koenigstag <koenigstag@gmail.com>
2024-05-04 00:01:17 +0300

    add(tests) add few tests for mapAsync and mapArrayAsync

--- packages/integration-test/src/classes/map-async.spec.ts
+++ packages/integration-test/src/classes/map-async.spec.ts
@@ -1,6 +1,7 @@
 import { classes } from '@automapper/classes';
 import {
     afterMap,
+    beforeMap,
     createMap,
     createMapper,
     forMember,
@@ -9,28 +10,69 @@ import {
 import { SimpleUserDto } from './dtos/simple-user.dto';
 import { SimpleUser } from './models/simple-user';
 
+async function asyncResolve<T>(value: T, delayMs = 1000): Promise<T> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(value);
+        }, delayMs);
+    });
+}
+
+const getFullname = (user: SimpleUser) => user.firstName + ' ' + user.lastName;
+
 describe('Map Async Classes', () => {
     const mapper = createMapper({ strategyInitializer: classes() });
 
-    it('should map', async () => {
+    it('should map async', async () => {
         createMap(
             mapper,
             SimpleUser,
             SimpleUserDto,
             forMember((d) => d.fullName, ignore()),
-            afterMap(async (_, destination) => {
-                const fullName = await Promise.resolve().then(
-                    () => 'Tran Chau'
-                );
+            beforeMap(async (source) => {
+                source.firstName = await asyncResolve(source.firstName);
+            }),
+            afterMap(async (source, destination) => {
+                const fullName = await asyncResolve(getFullname(source));
+
                 Object.assign(destination, { fullName });
             })
         );
 
+        const user = new SimpleUser('Chau', 'Tran');
+
         const dto = await mapper.mapAsync(
-            new SimpleUser('Chau', 'Tran'),
+            user,
             SimpleUser,
             SimpleUserDto
         );
-        expect(dto.fullName).toEqual('Tran Chau');
+        expect(dto.fullName).toEqual(user.firstName + ' ' + user.lastName);
     });
+
+    it('should map array async without calling afterMap', async () => {
+      createMap(
+          mapper,
+          SimpleUser,
+          SimpleUserDto,
+          forMember((d) => d.fullName, ignore()),
+          afterMap(async (source, destination) => {
+              const fullName = await asyncResolve(getFullname(source));
+
+              Object.assign(destination, { fullName });
+          })
+      );
+
+      const user = new SimpleUser('Chau', 'Tran');
+
+      const dtos = await mapper.mapArrayAsync(
+          [user],
+          SimpleUser,
+          SimpleUserDto
+      );
+
+      expect(dtos).toHaveLength(1);
+      expect(dtos[0].firstName).toEqual(user.firstName);
+      expect(dtos[0].lastName).toEqual(user.lastName);
+      expect(dtos[0].fullName).toEqual(undefined); // afterMap is not called
+  });
 });
