b2bae3751c30ffe34a6748446d7b8da675c983ac
Kaarel Raspel <kaarel@analyticalalley.com>
2024-09-03 17:53:00 +0200

    feat(core): Continue @koenigstag  initiative: improve type safety

--- packages/core/src/lib/utils/is-promise.ts
+++ packages/core/src/lib/utils/is-promise.ts
@@ -5,8 +5,6 @@
  */
 export function isPromise(value: unknown): value is Promise<unknown> {
     return (
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //            TS2339: Property then does not exist on type {}
-        typeof (value === null || value === void 0 ? void 0 : value.then) === 'function'
+        typeof (value === null || value === void 0 ? void 0 : (value as any).then) === 'function'
     );
 }
